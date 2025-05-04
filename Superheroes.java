6def03b80f7b9f44bba82c6b4b08194dd179bfe4
Xitizz11 <ghimire.k971496@stu.SanJac.edu>
2025-05-04 17:34:42 -0500

    Update Superheroes.java

--- Superheroes.java
+++ Superheroes.java
@@ -1,3 +1,26 @@
+class Superhero {
+    private String name;
+    private String ability;
+    private int powerLevel;
+
+    public Superhero(String name, String ability, int powerLevel) {
+        this.name = name;
+        this.ability = ability;
+        this.powerLevel = powerLevel;
+    }
+
+    public int getPowerLevel() {
+        return powerLevel;
+    }
+
+    public void displayHero() {
+        System.out.println("Hero: " + name);
+        System.out.println("Ability: " + ability);
+        System.out.println("Power Level: " + powerLevel);
+        System.out.println("------------------------");
+    }
+}
+
 public class SuperheroRoster {
     public static void main(String[] args) {
         // 1. Declare and initialize arrays for hero names, abilities, and power levels
